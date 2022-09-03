cadf78965b98bd84f0ee1154ce5a257677c48c00
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-03 12:48:30 +0200

    styled.h1 replaces className and children h1

--- src/Components/Titulo/index.jsx
+++ src/Components/Titulo/index.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import styled from "styled-components";
 
-const Titulo = ({ children }) => {
-  return <h1 className="titulo">{children}</h1>;
-};
+const Titulo = styled.h1`
+  color: grey;
+  padding: 25px 0;
+`
+
+// styled.h1 replaces className and children 
+// const Titulo = ({ children }) => {
+//   return <h1 className="titulo">{children}</h1>;
+// };
+
 export default Titulo;
