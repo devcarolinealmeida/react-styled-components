72451773e5e9231566cd5ffe01cf446ab94c3150
devcarolinealmeida <carolineitalia85@gmail.com>
2022-09-05 13:00:26 +0200

    refactor: BtnHeader with styled-component

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -3,6 +3,17 @@ import styled from "styled-components";
 import bank_logo from "../../assets/images/bank_logo.svg";
 import { firstColor } from "../UI/Variaveis";
 
+const BtnHeader = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+  color: ${(props) => (props.primary ? "white" : firstColor)};
+  background: ${(props) => (props.primary ? firstColor : "white")};
+`
+
 const StyledHeader = styled.nav`
   background-color: ${firstColor};
   display: flex;
@@ -11,7 +22,6 @@ const StyledHeader = styled.nav`
   height: 10vh;
   align-items: center;
 `
-
 const Logo = styled.img`
   height: 50px;
   width: 50px;
@@ -22,12 +32,12 @@ const Cabecalho = () => {
     <StyledHeader>
       <Logo src={bank_logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
-          Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
-          Sair
-        </a>
+        <BtnHeader href="https://google.com">
+          Help
+        </BtnHeader>
+        <BtnHeader primary href="https://google.com">
+          Exit
+        </BtnHeader>
       </div>
     </StyledHeader>
   );
